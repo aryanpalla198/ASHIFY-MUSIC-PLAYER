209dc70b3fd4378e73562ccdbf5202dcea713bfd
User <user@example.com>
2026-08-07 21:12:46 +0530

    Fix casing of deployment base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ashify-music-player/',
+  base: '/ASHIFY-MUSIC-PLAYER/',
   server: {
     port: 3000,
     host: true
